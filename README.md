e1acf9bde681f0bedd37afdb1480091f59ef80e0
Seth Griffin <therealsethgriffin@proton.me>
2025-08-28 11:46:47 -0500

    Added sql alchemy connection to app

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Quick Start
+=====================
+flask run --port 5001
+
+
+Next:
+====================
+-- Add content database (maria nosql, dml management tool, ddl, orm)
+
--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,17 @@ from flask import Flask
 app = Flask(__name__,
             static_url_path='/web/',
             static_folder='web/static',
-            template_folder='web/templates')
+            template_folder='web/templates',
+            )
 
 from app import routes
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+db_user = 'dev'
+db_pass = ''
+db_ip = 'localhost'
+db_name = 'blog'
+engine = create_engine('mysql+mysqlconnector://{}:{}@{}/{}/'.format(db_user, db_pass, db_ip, db_name))
+
+if not database_exists(engine.url): create_database(engine.url)
