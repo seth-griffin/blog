1ce748e95cd40fcd438ee72487d31fc65fcb01b8
Seth Griffin <therealsethgriffin@proton.me>
2026-01-27 17:20:58 -0600

    Added data import files for existing articles, refactored routes and templates

--- app/blueprints/blog/routes.py
+++ app/blueprints/blog/routes.py
@@ -4,27 +4,21 @@ from app.blueprints.db.models import Post
 
 blog = Blueprint("blog", __name__, template_folder="web/static/templates")
 
-
-@blog.route("/", methods=['GET'])
+@blog.route("/", methods=["GET"])
 @blog.route("/index")
 def index():
     return render_template("index.html")
 
 
-@blog.route("/posts", methods=['GET'])
-def posts():
-    return render_template("post-list.html")
-
-
 # Ex: programming/functional-programming/lisp/scheme/arc/2025/12/11/introduction-arc-programming-language.html
 @blog.route(
     "/post/<path:categories>/<int:year>/<int:month>/<int:day>/<string:title>",
-    methods=['GET']
+    methods=["GET"],
 )
 def post(categories, year, month, day, title):
     return render_template("post.html")
 
 
-@blog.route("/about", methods=['GET'])
+@blog.route("/about", methods=["GET"])
 def about():
     return render_template("about.html")
